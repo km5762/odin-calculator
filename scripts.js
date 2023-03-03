6961a9992179b774e388a093cde526d9ef9b8473
km5762 <km5762@DESKTOP-587P2TU>
2023-03-03 13:24:32 -0500

    Added button functionality

--- scripts.js
+++ scripts.js
@@ -1,8 +1,94 @@
-let values = document.querySelectorAll('.buttonrow .value');
-let viewPortText = document.querySelector('.viewport span');
+const values = document.querySelectorAll('.buttonrow .value');
+const operators = document.querySelectorAll('.buttonrow .operator');
+const viewPortText = document.querySelector('.viewport span');
+const clear = document.querySelector('#C');
+const memClear = document.querySelector('#AC');
+const add = document.querySelector('#add');
+const equals = document.querySelector('#equals');
+
+let val1 = 'await';
+let val2 = 'await';
+let currentOperation = 'await';
 
 values.forEach(value => {
     value.addEventListener('click', () => {
+        if (val2 == parseFloat(viewPortText.textContent) || val1 == parseFloat(viewPortText.textContent)) {
+            clearDisplay();
+        }
         viewPortText.textContent += value.textContent;
     })
+});
+
+clear.addEventListener('click', clearDisplay);
+
+memClear.addEventListener('click', reset);
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if (val2 == 'await') {
+            val2 = parseFloat(viewPortText.textContent);
+        }
+        evaluate();
+        currentOperation = operator.textContent;
+        updateDisplay();
+    });
+});
+
+equals.addEventListener('click', () => {
+    if (val2 == 'await') {
+        val2 = parseFloat(viewPortText.textContent);
+    }
+    evaluate();
+    currentOperation = 'await';
+    updateDisplay();
 })
+
+function clearDisplay() {
+    viewPortText.textContent = '';
+}
+
+function evaluate() {
+    if (val1 === 'await') {
+        val1 = val2;
+    } else if (val2 != 'await') {
+        switch(currentOperation) {
+            case '+':
+                val1 = val1 + val2;
+                break;
+            case '-':
+                val1 = val1 - val2;
+                break;
+            case 'x':
+                val1 = val1 * val2;
+                break;
+            case '÷':
+                if (val2 != 0) {
+                    val1 = val1 / val2;
+                } else {
+                    window.alert('Error: Divsion by 0');
+                    val1 = 0;
+                }
+                break;
+            case '%':
+                val1 = val1 % val2;
+                break;
+        }
+    }
+    val2 = 'await';
+}
+
+function updateDisplay() {
+    if ((val1 % 1) != 0) {
+        viewPortText.textContent = val1.toFixed(3);
+    } else {
+        viewPortText.textContent = val1;
+    }
+}
+
+function reset() {
+    val1 = 'await';
+    val2 = 'await';
+    currentOperation = 'await';
+    clearDisplay(); 
+}
+
